f8fe95a3c2f941f21e8423e4a87c7db022430fcb
vpate48 <49735598+vpate48@users.noreply.github.com>
2020-03-01 17:10:02 -0600

    Add files via upload

---
+++ Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let rec contains S =
+  match S with 
+  |[]-> true
+  | hd::tl -> if(hd = false) then false else contains tl
+
+let rec superset S L = 
+  match S with
+  | []-> true
+  | hd::tl -> if ((contains ( (L |> List.map (fun x -> x <>hd))) = false )) then false else superset tl L
+
+[<EntryPoint>]
+
+    printfn("%A") (superset [3; 1; 2] [1; 4; 2; 0; 3])
+    0 // return an integer exit code
